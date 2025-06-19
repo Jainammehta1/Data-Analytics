42631d777c743205002b01134423ff14be1eb3a3
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:33:57 +0000

    Lists

---
+++ Python Basics/Lists/Slicing/main.py
@@ -0,0 +1,11 @@
+fruits=["apple","banana",12,14,15]
+print(fruits)
+print(type(fruits))
+
+#SLICING LISTS
+#Accessing the element
+a=["Iron man","Hulk","Superman","Spider man"]
+print(a[2])
+print(a[1:3]) #Multiple elements accessing
+print(a[::2]) #Skip the element
+print(a[::-1]) #Reverse the list
