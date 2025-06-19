0f52c7765fa96ad75ef8ce35c754d882add22903
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:37:00 +0000

    String Manipulation

---
+++ Python Basics/String Manipulation/main.py
@@ -0,0 +1,35 @@
+#Length
+a="Hello world"
+print(len(a))
+
+#Count
+b="Hello World"
+print(b.count("o"))
+
+#UPPER
+c="hello world"
+print(c.upper())
+
+#Lower
+d="HELLO WORLD"
+print(d.lower())
+
+#index
+e="Hello world"
+print(e.index("H"))
+
+#Capitalize
+f="hello world"
+print(f.capitalize())
+
+#casefold
+g="Hello world"
+print(g.casefold())
+
+#find
+h="My name is jainam"
+print(h.find("j"))
+
+#center
+name="John"
+print(name.center(10))
