033c97484406a436cd641e9241a2123f390bc292
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:36:43 +0000

    Modules

---
+++ Python Basics/Modules/main.py
@@ -0,0 +1,33 @@
+# Datetime
+import datetime
+import math
+
+x=datetime.datetime.now()
+print(x)
+
+#Random module
+import random
+y=random.randint(1,10)
+print(y)
+
+#Math Module
+a=max(12,24,25) #MAX
+print(a)
+
+b=min(1,2,4) #MIN
+print(b)
+
+c=pow(2,4) #Power
+print(c)
+
+d=math.sqrt(49)
+print(d)
+
+e=abs(-12) #Converts the negative value in positive
+print(e)
+
+f=math.ceil(2.4) #ceil
+print(f)
+
+g=math.floor(2.4)
+print(g)
