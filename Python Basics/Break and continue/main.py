496526b2043ea6a3467449a2760a50c4f36e983b
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:32:00 +0000

    Break and Continue

---
+++ Python Basics/Break and continue/main.py
@@ -0,0 +1,15 @@
+#CONTINUE
+for i in range(1,11):
+    if i==5:
+        continue
+    else:
+        print(i)
+
+
+#BREAK
+
+for n in range(1,11):
+    if n==7:
+        break
+    else:
+        print(n)
