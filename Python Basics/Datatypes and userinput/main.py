563f9ed18d6385ab36027bf1ec747aa15998c36d
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:31:04 +0000

    Datatypes and Userinput

---
+++ Python Basics/Datatypes and userinput/main.py
@@ -0,0 +1,11 @@
+name=input("Enter your name here")
+print(name) #String
+
+age=int(input("Enter your age "))
+print(age) #Integer
+
+height=float(input("Enter your height"))
+print(height) #Float
+
+exp1=eval(input("Enter your expression"))
+print(exp1) #Evaluates the expression
