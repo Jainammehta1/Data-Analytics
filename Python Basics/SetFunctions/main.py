574a4fc1efe01fc8a2c56c18e86120d09e25d02a
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:36:24 +0000

    Set Functions

---
+++ Python Basics/SetFunctions/main.py
@@ -0,0 +1,17 @@
+a={"Ironman","Hulk","Spiderman"}
+
+#Add
+a.add("Captain America")
+print(a)
+
+#pop
+a.pop() #RANDOM ELEMENT REMOVAL
+print(a)
+
+#remove
+a.remove("Hulk") #PARTICULAR ELEMENT REMOVAL
+print(a)
+
+#Copy
+b=a.copy()
+print(b)
