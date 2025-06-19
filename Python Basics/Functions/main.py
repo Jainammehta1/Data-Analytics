4cffef3375e63287201c24dab3b66afd58df5e37
Jainam Mehta <129773752+Jainammehta1@users.noreply.github.com>
2025-06-19 15:32:11 +0000

    Functions

---
+++ Python Basics/Functions/main.py
@@ -0,0 +1,16 @@
+def hello(): #DEFINATION------
+    print("Hello world")
+
+hello() #FUNCTIONCALL------
+
+def add():
+    x=56
+    y=45
+    print(x+y)
+add()
+
+#Parameters and Arguements ---------
+
+def sub(x,y):
+    print(x-y)
+sub(25,20)
